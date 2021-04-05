6e232d1ec64d54557e4387138b07d362707984f6
Karen Estefany Lopez Segura - Ceiba Software <karen.lopez@ceiba.com.co>
2021-04-04 20:33:09 -0500

    agregando decoraciones y precio de reservas

--- e2e/src/test/reserva.e2e-spec.ts
+++ e2e/src/test/reserva.e2e-spec.ts
@@ -47,10 +47,18 @@ describe('workspace-project Reserva', () => {
     });
 
     it('Deberia eliminar reserva', () => {
-        const NUMERO_IDENTIFICACION_PERSONA = '9999000';
-        
+        const NUMERO_IDENTIFICACION_PERSONA = '9999001';
+        const NOMBRE_PERSONA = 'persona de pruebas';
+        const FECHA_RESERVA = '27-04-2021';
+
         page.navigateTo();
         navBar.clickBotonReservas();
+        reserva.clickCrearReserva();
+        reserva.ingresarNumeroDocumentoPersona(NUMERO_IDENTIFICACION_PERSONA);
+        reserva.ingresarNombrePersona(NOMBRE_PERSONA);
+        reserva.ingresarFechaReserva(FECHA_RESERVA);
+        reserva.clickBotonCrearReserva();
+        reserva.clickBotonConfirmar();
         reserva.clickConsultarReservas();
         reserva.ingresarNumeroDocumentoConsulta(NUMERO_IDENTIFICACION_PERSONA);
         reserva.clickBotonConsultarReservas();
